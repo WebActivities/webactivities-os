40341b86885f7b80c266c33a1bceb5a9d8d48c9f
Simone Vellei <Simone Vellei@SIMONE.homenet.telecomitalia.it>
2014-05-07 23:35:57 +0200

    Corretto esempio

--- WebActivities/apps/app1/activities/activity1/activity1.js
+++ WebActivities/apps/app1/activities/activity1/activity1.js
@@ -2,6 +2,8 @@ var Activity1 = function(ctx) {
 
 	var container = $("<div></div>");
 	var header = $("<h1>").text("This is the first application").appendTo(container);
+	$("<input type=\"text\" class=\"form-control\" placeholder=\"Inserisci qualcosa per vedere se mantiene lo stato\">").appendTo(container);
+	$("<hr />").appendTo(container);
 	var btn1 = $("<button type=\"button\" class=\"btn btn-sm btn-primary\">Chiudi</button>").appendTo(container);
 	btn1.click(function() {
 		ctx.stop();
