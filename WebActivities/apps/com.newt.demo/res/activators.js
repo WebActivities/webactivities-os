221fd3af1624ae37709015312c226aae054352a9
Simone Vellei <Simone Vellei@192.168.35.155>
2014-05-19 14:20:01 +0200

    Aggiunti parti demo (parziale)

--- WebActivities/apps/com.newt.demo/res/activators.js
+++ WebActivities/apps/com.newt.demo/res/activators.js
@@ -23,6 +23,44 @@ var DashboardActivator = function(context, parameters) {
 		$("#messagesLnk", ui).click(function() {
 			context.newActivityIntent("com.newt.demo", "messages", {}).start();
 		});
+		$("#manageCustomerLnk", ui).click(function() {
+			context.newActivityIntent("com.newt.demo", "customerSearch", {}).start();
+		});
+		$("#manageProjectLnk", ui).click(function() {
+			context.newActivityIntent("com.newt.demo", "projectSearch", {}).start();
+		});
+		$("#newProjectLnk", ui).click(function() {
+			context.newActivityIntentAsPopup("com.newt.demo", "projectEdit", {}).start({
+				relativeTo : this
+			});
+		});
+		$("#newCustomerLnk", ui).click(function() {
+			context.newActivityIntentAsPopup("com.newt.demo", "customerEdit", {}).start({
+				relativeTo : this
+			});
+		});
+		$("#projectBtn", ui).click(function() {
+			context.newActivityIntentAsPopup("com.newt.demo", "projectSearch", {}).start({
+				relativeTo : this
+			});
+		});
+		$("#customerBtn", ui).click(function() {
+			context.newActivityIntentAsPopup("com.newt.demo", "customerSearch", {}).start({
+				relativeTo : this
+			});
+		});
+		var f1 = context.createFragment("com.newt.demo", "customerSearch");
+		$("#customerFragment", ui).append(f1.getComponent().css({
+			width : "100%",
+			height : "200px",
+		}));
+		f1.start();
+		var f2 = context.createFragment("com.newt.demo", "projectSearch");
+		$("#projectFragment", ui).append(f2.getComponent().css({
+			width : "100%",
+			height : "300px",
+		}));
+		f2.start();
 	});
 
 };
@@ -45,6 +83,34 @@ var MessageReaderActivator = function(context, parameters) {
 		$("#close-message", ui).click(function() {
 			context.stop();
 		});
+	});
+
+};
+
+var CustomerSearchActivator = function(context, parameters) {
+
+	context.prepareView(context.resolveUrl("view/customer-search.html")).then(function(ui) {
+	});
+
+};
+
+var CustomerEditActivator = function(context, parameters) {
+
+	context.prepareView(context.resolveUrl("view/customer-edit.html")).then(function(ui) {
+	});
+
+};
+
+var ProjectSearchActivator = function(context, parameters) {
+
+	context.prepareView(context.resolveUrl("view/project-search.html")).then(function(ui) {
+	});
+
+};
+
+var ProjectEditActivator = function(context, parameters) {
+
+	context.prepareView(context.resolveUrl("view/project-edit.html")).then(function(ui) {
 	});
 
 };
